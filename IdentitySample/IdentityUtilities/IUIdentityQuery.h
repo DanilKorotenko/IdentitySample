e42967e3255bc6f104939ad9820453c1f25524df
Danil Korotenko <danil.korotenko@gmail.com>
2024-08-08 18:59:09 +0300

    Added identity utilities.

---
+++ IdentitySample/IdentityUtilities/IUIdentityQuery.h
@@ -0,0 +1,16 @@
+//
+//  IUIdentityQuery.h
+//  IdentitySample
+//
+//  Created by Danil Korotenko on 8/8/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IUIdentityQuery : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ IdentitySample/IdentityUtilities/IUIdentityQuery.m
@@ -0,0 +1,12 @@
+//
+//  IUIdentityQuery.m
+//  IdentitySample
+//
+//  Created by Danil Korotenko on 8/8/24.
+//
+
+#import "IUIdentityQuery.h"
+
+@implementation IUIdentityQuery
+
+@end
