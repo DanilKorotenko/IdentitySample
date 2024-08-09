2c5528294445a38f42fcb39c6b47db1b5f526639
Danil Korotenko <danil.korotenko@gmail.com>
2024-08-09 18:23:54 +0300

    Added cpp adapter.

--- cppidentity/main.cpp
+++ cppidentity/main.cpp
@@ -6,9 +6,32 @@
 //
 
 #include <iostream>
+#include "../IdentityUtilities/IUIdentityAdapter.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, const char * argv[])
+{
+    std::cout << "Hello, cpp identity!" << std::endl;
+
+    std::string userName = "testUser";
+    std::string password = "pass123456";
+    std::string errorDescription;
+    if (IUIdentityUserExist(userName))
+    {
+        std::cout << "user exist" << std::endl;
+        std::cout << "delete user" << std::endl;
+        if (!IUIdentityDeleteUser(userName, errorDescription))
+        {
+            std::cout << "delete user error: " << errorDescription.c_str() << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "add user" << std::endl;
+        if (!IUIdentityAddAdminUser(userName, password, errorDescription))
+        {
+            std::cout << "add user error: " << errorDescription.c_str() << std::endl;
+        }
+    }
+
     return 0;
 }
