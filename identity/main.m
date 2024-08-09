e24f4ce2c2eb67a96a17ab1f3948c807758dd284
Danil Korotenko <danil.korotenko@gmail.com>
2024-08-09 14:09:29 +0300

    Added command linae tool target.

---
+++ identity/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  identity
+//
+//  Created by Danil Korotenko on 8/9/24.
+//
+
+#import <Foundation/Foundation.h>
+#import "../IdentityUtilities/IUIdentityQuery.h"
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        NSLog(@"Hello, identity!");
+
+    }
+    return 0;
+}
